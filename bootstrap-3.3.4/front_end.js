199075fc32f8e3757e8a0d7564896f6e554a4017
unknown <n0268537@VDDP03P-CC74376.lm.lmig.com>
2015-07-23 13:04:55 -0400

    Refactoring Project Format

--- bootstrap-3.3.4/front_end.js
+++ bootstrap-3.3.4/front_end.js
@@ -104,7 +104,7 @@ $('#collapseOne, #collapseTwo, #collapseThree, #collapseFour').collapse("hide");
 
 // Anchor Scroll
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('a[href*=#display_projects]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
